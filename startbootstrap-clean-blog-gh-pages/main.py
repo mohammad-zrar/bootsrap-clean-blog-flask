d0cae57a030877ff35c6063478468ac763ed3f07
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2023-01-18 23:02:39 +0300

    commit of day 67

--- startbootstrap-clean-blog-gh-pages/main.py
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -1,51 +1,77 @@
-from flask import Flask, render_template, request
-from post import Post
 import requests
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
 
-posts = requests.get("https://api.npoint.io/3fd2ae95189b864f5f49").json()
-post_objects = []
-for post in posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["supervisor"], post["year"])
-    post_objects.append(post_obj)
+# # Delete this code:
+# import requests
+# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+# #CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# #CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+# #WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = StringField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
 
 
 @app.route('/')
-def home():
-    return render_template("index.html", all_posts=post_objects)
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-
-@app.route('/form-entry', methods=['POST', 'GET'])
-def receive_data():
-    if request.method == "POST":
-        data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
-        return render_template("contact.html", submited=True)
-    return render_template("contact.html", submited=False)
+def get_all_posts():
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
 
 
 @app.route("/post/<int:index>")
 def show_post(index):
+    posts = requests.get("https://api.npoint.io/3fd2ae95189b864f5f49").json()
     requested_post = None
-    for blog_post in post_objects:
-        if blog_post.id == index:
+    for blog_post in posts:
+        if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("new-post")
+def new_post():
+    return render_template("make-post.html")
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
