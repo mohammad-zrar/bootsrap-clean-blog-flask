8c65f3d31298d4fc027516793fa188b2f578959e
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2023-02-07 15:12:56 +0300

    Organize file and folders

--- startbootstrap-clean-blog-gh-pages/post.py
+++
@@ -1,7 +0,0 @@
-class Post:
-    def __init__(self, post_id, title, subtitle, supervisor, year):
-        self.id = post_id
-        self.title = title
-        self.subtitle = subtitle
-        self.supervisor = supervisor
-        self.year = year
